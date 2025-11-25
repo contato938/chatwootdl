07be64ccbac27f4f7d5b9dbc6c87c72b83c1498c
marcoaureliorosa <contato@dynamicslabs.com.br>
2025-11-25 19:58:17 -0300

    Update account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -92,6 +92,8 @@ class Account < ApplicationRecord
   has_many :telegram_channels, dependent: :destroy_async, class_name: '::Channel::Telegram'
   has_many :twilio_sms, dependent: :destroy_async, class_name: '::Channel::TwilioSms'
   has_many :twitter_profiles, dependent: :destroy_async, class_name: '::Channel::TwitterProfile'
+  has_many :sales_pipelines, dependent: :destroy_async
+  has_many :sales_pipeline_stages, dependent: :destroy_async
   has_many :users, through: :account_users
   has_many :web_widgets, dependent: :destroy_async, class_name: '::Channel::WebWidget'
   has_many :webhooks, dependent: :destroy_async
