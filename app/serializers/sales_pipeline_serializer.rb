69f096e2b373494997f590e7c19399768679e739
marcoaureliorosa <contato@dynamicslabs.com.br>
2025-11-24 23:14:46 -0300

    fix deploy

--- app/serializers/sales_pipeline_serializer.rb
+++
@@ -1,5 +0,0 @@
-class SalesPipelineSerializer < ActiveModel::Serializer
-  attributes :id, :name, :created_at, :updated_at
-
-  has_many :sales_pipeline_stages
-end
--- app/serializers/sales_pipeline_stage_serializer.rb
+++
@@ -1,6 +0,0 @@
-class SalesPipelineStageSerializer < ActiveModel::Serializer
-  attributes :id, :name, :color, :position, :is_default, :is_closed_won, :is_closed_lost, :created_at, :updated_at
-
-  belongs_to :label
-  belongs_to :sales_pipeline
-end
